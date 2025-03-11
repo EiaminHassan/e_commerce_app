4781bad75de45b0fc2ac46258653ab95b6f8977b
EiaminHassan <shanto15-5251@diu.edu.bd>
2025-03-11 10:06:30 +0600

    initial_structure

---
+++ lib/utils/consts/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class FColors {
+  static const primary = Color(0xFF5CC936);
+  static const primnaryOrange = Color(0xFFFFC120);
+  static const primnaryNavy = Color(0xFF0C1A30);
+  static const primnaryWhite = Color(0xFFFFFFFF);
+}
