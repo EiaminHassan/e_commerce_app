9fd9dc7fe2989ed924431aa7d6c893737eed7a4c
EiaminHassan <shanto15-5251@diu.edu.bd>
2025-03-18 10:01:11 +0600

    bottom_bar_click

--- lib/screens/home/page/home.dart
+++ lib/screens/home/page/home.dart
@@ -4,8 +4,27 @@ import 'package:icons_plus/icons_plus.dart';
 
 import '../../../common/widgets/app_bar.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+  final List<Widget> _screens = [
+    Container(height: double.infinity, color: Colors.amber),
+    Container(height: double.infinity, color: Colors.green),
+    Container(height: double.infinity, color: Colors.blue),
+    Container(height: double.infinity, color: Colors.red),
+  ];
+
+  void _onTapMethod(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +34,11 @@ class Home extends StatelessWidget {
         firstIcon: Iconsax.notification_bing_outline,
         //backButton: true,
       ),
-      bottomNavigationBar: BottomBar(),
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomBar(
+        currentIndex: _currentIndex,
+        onTapMethod: _onTapMethod,
+      ),
     );
   }
 }
--- lib/screens/home/widgets/bottom_nav_bar.dart
+++ lib/screens/home/widgets/bottom_nav_bar.dart
@@ -2,11 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
 
 class BottomBar extends StatelessWidget {
-  const BottomBar({super.key});
+  const BottomBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTapMethod,
+  });
+
+  final int currentIndex;
+  final Function(int) onTapMethod;
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTapMethod,
       type: BottomNavigationBarType.fixed,
       items: [
         BottomNavigationBarItem(
